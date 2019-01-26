f8daebead12fc6086c105bc386d63bd9cf72ae78
Yusong Hsu <sky172839465@gmail.com>
2019-01-26 11:43:30 +0800

    Add multiple test

--- codecept/bs.config.js
+++ codecept/bs.config.js
@@ -2,6 +2,7 @@ const now = new Date()
 const startDate = `${now.getFullYear()}/${now.getMonth() + 1}/${now.getDate()}`
 const startTime = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`
 const {
+  PROJECT_NAME = 'Test app',
   BS_USER,
   BS_KEY,
   BASE_URL = 'https://sky172839465.github.io/test-app',
@@ -10,25 +11,60 @@ const {
   IS_TEST_LOCAL = false
 } = process.env
 
+const getBrowserConfig = browserName => ({
+  'browserstack.local': IS_TEST_LOCAL,
+  'browserstack.debug': true,
+  'browserstack.networkLogs': true,
+  project: `${PROJECT_NAME}(${browserName})`,
+  build: `${PROJECT_NAME} in ${browserName} ${TRAVIS_BUILD_NUMBER} ${TRAVIS_JOB_NUMBER}`
+})
+
 exports.config = {
   name: 'test-app',
   tests: '../src/__e2e__/src/**/**.js',
   output: '../report',
+  multiple: {
+    basic: {
+      browsers: [
+        {
+          browser: 'safari',
+          desiredCapabilities: {
+            os: 'OS X',
+            os_version: 'High Sierra',
+            ...getBrowserConfig('Mac Safari')
+          }
+        },
+        {
+          browser: 'chrome',
+          desiredCapabilities: {
+            os: 'OS X',
+            os_version: 'High Sierra',
+            ...getBrowserConfig('Mac Chrome')
+          }
+        },
+        {
+          browser: 'chrome',
+          desiredCapabilities: {
+            os: 'Windows',
+            os_version: '10',
+            ...getBrowserConfig('Windows Chrome')
+          }
+        }
+      ]
+    }
+  },
   helpers: {
     WebDriverIO: {
       url: BASE_URL,
       user: BS_USER,
       key: BS_KEY,
       desiredCapabilities: {
-        'browserstack.local': IS_TEST_LOCAL,
-        'browserstack.debug': true,
-        project: 'Test app',
-        build: `Test app ${TRAVIS_BUILD_NUMBER} ${TRAVIS_JOB_NUMBER}`,
         os: 'Windows',
         os_version: '10',
-        resolution: '1920x1080'
+        ...getBrowserConfig('Windows Chrome')
       },
-      browser: 'chrome'
+      browser: 'chrome',
+      windowSize: 'maximize'
     }
   },
   include: {
