f7c2c3ad12e0ddcdb2bbef4691b0dbb336b74117
Yusong Hsu <sky172839465@gmail.com>
2019-02-01 20:32:35 +0800

    Update travis script

---
+++ src/__tests__/src/component/Common/ResultNotFound/index.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import ResultNotFound from '../../../../../component/Common/ResultNotFound'
+
+const props = {
+  className: 'testClassName',
+  onDelete: jest.fn(),
+  children: 'result not found'
+}
+
+describe('/component/Common/ResultNotFound', () => {
+  const wrapper = shallow(<ResultNotFound {...props} />)
+  it('test not found message content', () => {
+    const message = wrapper.find(`[className*="${props.className}"]`)
+    const closeMsgBtn = wrapper.find('button[className="delete"]')
+    closeMsgBtn.simulate('click')
+    expect(message).toHaveLength(1)
+    expect(message.text()).toEqual(props.children)
+    expect(props.onDelete).toHaveBeenCalled()
+  })
+})
---
+++ src/__tests__/src/component/Page/NotFound/index.test.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import NotFound from '../../../../../component/Page/NotFound'
+
+const props = {
+  message: 'test not found'
+}
+
+describe('/component/Page/NotFound', () => {
+  it('test default not found content', () => {
+    const wrapper = shallow(<NotFound />)
+    expect(wrapper.text()).toEqual('Oops! Not found page.')
+  })
+
+  it('test custom not found message content', () => {
+    const wrapper = shallow(<NotFound {...props} />)
+    expect(wrapper.text()).toEqual(props.message)
+  })
+})
