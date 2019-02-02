7690ad500016e9d93df9379629984a43c2509d08
Yusong Hsu <sky172839465@gmail.com>
2019-02-02 15:40:41 +0800

    Fix script

--- src/__e2e__/src/Page/NotFound.js
+++ src/__e2e__/src/Page/NotFound.js
@@ -5,7 +5,7 @@ const pageObject = require('../../pageObject')
 Scenario('default page', (I) => {
   I.amOnPage('/test-app/#/THIS_IS_INVAILD_ROUTE')
   I.waitForVisible(pageObject.appLink)
-  I.see('Test App1', pageObject.appLink)
+  I.see('Test App', pageObject.appLink)
   I.seeElement(pageObject.notFound)
   I.see('Oops! Not found page.', pageObject.notFound)
 })
